069176e9148585a3b4d609237f11f65ca1dc93df
Jon Palmisciano <jp@jonpalmisc.com>
2021-12-10 12:09:44 -0500

    Add interactive style selection

--- clangformat.py
+++ clangformat.py
@@ -5,8 +5,10 @@ import subprocess
 
 
 class ClangFormatCommand(sublime_plugin.TextCommand):
-    def run(self, edit: sublime.Edit):
-        command = ["clang-format", "-style", "LLVM", "-"]
+    """Format the current buffer."""
+
+    def run(self, edit: sublime.Edit, style: str):
+        command = ["clang-format", "-style", style, "-"]
 
         process = subprocess.Popen(
             command,
@@ -15,14 +17,22 @@ class ClangFormatCommand(sublime_plugin.TextCommand):
             stdin=subprocess.PIPE,
         )
 
-        buffer = self.view.substr(sublime.Region(0, self.view.size()))
+        global_region = sublime.Region(0, self.view.size())
+        buffer = self.view.substr(global_region)
 
         result, error = process.communicate(buffer.encode("utf-8"))
         if error:
+            sublime.error_message("Failed to format buffer.")
             return
 
-        self.view.replace(
-            edit, sublime.Region(0, self.view.size()), result.decode("utf-8")
-        )
+        self.view.replace(edit, global_region, result.decode("utf-8"))
 
-        pass
+    def input(self, args):
+        return StyleInputHandler()
+
+
+class StyleInputHandler(sublime_plugin.ListInputHandler):
+    """Input handler to choose format presets."""
+
+    def list_items(self):
+        return ["Local", "LLVM", "WebKit"]
